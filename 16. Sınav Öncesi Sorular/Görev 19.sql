c4a81cd92566c49b597b980fd45902fab5919968
omERdem555 <omererdem555@outlook.com>
2026-05-26 14:08:39 +0300

    Görev 19 yapıldı

---
+++ 16. Sınav Öncesi Sorular/Görev 19.sql
@@ -0,0 +1,9 @@
+SELECT k.ad, k.soyad --Müşteri olan kullanıcılar
+FROM Musteri m
+JOIN Kullanici k ON m.kullanici_id = k.kullanici_id
+WHERE m.musteri_id NOT IN (
+    SELECT DISTINCT musteri_id FROM Siparis
+)
+
+Select k.ad, k.soyad --Bütün kullanıcılar
+From Kullanici as k
