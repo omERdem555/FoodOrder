ce8e10a068b788ce6ba43a1d702285890ba52c9b
omERdem555 <omererdem555@outlook.com>
2026-05-26 08:13:36 +0300

    Görev 13 yapıldı

---
+++ 16. Sınav Öncesi Sorular/Görev 13.sql
@@ -0,0 +1,5 @@
+Select mu.ad as 'Ürün Adı', Count(*) as 'Toplam Satış Adedi'
+From MenuUrun as mu
+Join SiparisDetay as sd On mu.urun_id = sd.urun_id
+Group By sd.urun_id, mu.ad
+Order By [Toplam Satış Adedi] Desc
