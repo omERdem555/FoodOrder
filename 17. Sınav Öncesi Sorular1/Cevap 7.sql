df435d465cf0a1076112c22a788252ec6f71e10e
omERdem555 <omererdem555@outlook.com>
2026-06-04 22:30:18 +0300

    Soru 7 yapıldı

---
+++ 17. Sınav Öncesi Sorular1/Cevap 7.sql
@@ -0,0 +1,6 @@
+Select k.ad, k.soyad, Count(s.siparis_id) as toplam_siparis
+From Musteri as m 
+Join Siparis as s on m.musteri_id = s.musteri_id
+Join Kullanici as k on m.kullanici_id = k.kullanici_id
+Group By k.ad, k.soyad
+Having Count(s.siparis_id) > 3
